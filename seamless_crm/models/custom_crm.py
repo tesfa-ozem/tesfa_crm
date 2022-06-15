3cb86a3504ba40eb566e3841772fc5e3082165ad
tesfa <alphatesfa789@gmail.com>
2022-06-15 06:45:54 +0300

    add custom fields

--- seamless_crm/models/custom_crm.py
+++ seamless_crm/models/custom_crm.py
@@ -14,6 +14,11 @@ class CustomCrm(models.Model):
     next_step = fields.Char('Next step')
     contract_date = fields.Char('Contract')
     feedback = fields.Char('Feedback')
+    decision_maker = fields.Char('Who\'s the decision maker')
+    documentation_needed = fields.Char('Documentations needed by client to close')
+    needed_to_close = fields.Char('What is needed to close a sale')
+    process_to_close = fields.Char('Process to close')
+    status = fields.Char('Status')
     # tag_ids = fields.Many2many(
     #     'crm.tag', 'crm_tag_rel', 'crm.custom.tag_ids', 'tag_id', string='Tags',
     #     help="Classify and analyze your lead/opportunity categories like: Training, Service")
